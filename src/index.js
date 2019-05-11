9bdef92e78dee6b26270847ef64b18983eade1bb
Georgy Marchuk <gmarcuk@gmail.com>
2019-05-11 17:22:13 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import Plugin from '@swup/plugin';
+
+export default class BodyClassPlugin extends Plugin {
+	name = 'SwupBodyClassPlugin';
+
+	constructor(options) {
+		super();
+
+		const defaultOptions = {
+			prefix: ''
+		};
+
+		this.options = {
+			...defaultOptions,
+			...options
+		};
+	}
+
+	mount() {
+		this.swup.on('contentReplaced', () => {
+			const page = this.swup.cache.getCurrentPage();
+
+			// remove old classes
+			document.body.className.split(' ').forEach((className) => {
+				if (this.isValidClassName(className)) {
+					document.body.classList.remove(className);
+				}
+			});
+
+			// add new classes
+			if (page.pageClass !== '') {
+				page.pageClass.split(' ').forEach((className) => {
+					if (this.isValidClassName(className)) {
+						document.body.classList.add(className);
+					}
+				});
+			}
+		});
+	}
+
+	isValidClassName(className) {
+		return className !== '' && className.includes(this.options.prefix);
+	}
+}
